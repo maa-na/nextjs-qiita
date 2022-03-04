ac9ccab48f164a06eb082e9622ebc2ebce907d53
maa-na <maa-na@users.noreply.github.com>
2022-03-05 01:04:36 +0900

    next sample

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-center">
+      <div>TEST</div>
+    </div>
+  );
+};
+
+export default Home;
